bd5d5a6067dd9d46472f13284f1c6ccebd8e0aea
flowp <florentpoux98@gmail.com>
2017-05-24 10:44:49 +0200

    Script upload v1

--- scripts/save_on_ftp.sh
+++ scripts/save_on_ftp.sh
@@ -1,1 +1,16 @@
-scp <file to upload> flowpfrgbx@ftp.cluster017.ovh.net:/www/secureasy/upload/
+#!/bin/sh
+HOST='ftp.cluster017.ovh.net'
+USER='flowpfrgbx'
+PASSWD='vsfuQHfrsfjU'
+WAY='/www/secureasy/upload/'
+FILE='test.txt'
+
+ftp -n $HOST <<END_SCRIPT
+quote USER $USER
+quote PASS $PASSWD
+binary
+cd /www/secureasy/upload
+put $FILE
+quit
+END_SCRIPT
+exit 0
